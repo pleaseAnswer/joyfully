a0bea038585e707318d8348ae83ef93b4fc70164
qiunanyan <992679600@qq.com>
2019-11-15 21:49:12 +0800

    完成登录注册

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import cart from './cart';
+import common from './common';
 Vue.use(Vuex);
 
 
 let store = new Vuex.Store({
-   modules:{
-       cart
-   }
+    modules: {
+        cart,
+        common
+    }
 })
 
 
