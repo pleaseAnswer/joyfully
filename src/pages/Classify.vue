9d1fe950f4e10c9aa3f9b81e1854b06486202582
LiZhouPing <1627339080@qq.com>
2019-11-15 10:41:39 +0800

    第一次提交

--- src/pages/Classify.vue
+++ src/pages/Classify.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <div>
-    <!-- 输入框 -->
-    <van-search placeholder="去喜地，逛世界" v-model="value"/>
+      <!-- 输入框 -->
+      <van-search placeholder="去喜地，逛世界" v-model="value" />
     </div>
     <div>
       <el-tabs type="border-card" tabPosition="left">
-      <el-tab-pane label="饮食厨房">
-        <ul class="">
-          <li class="list" v-for="item in 9" :key="item">
-            <img src="https://4-image.xidibuy.com/shop/shop.b898da88e7cd463f50914c5d8b2828358bbb67b48ac1bf540b9ea2f179c2bb0b.jpeg/1500x1500/240/webp" alt="">
-            <p>锅具</p>
-          </li>
-        </ul>
-      </el-tab-pane>
-      <el-tab-pane label="母婴用品">母婴用品</el-tab-pane>
-      <el-tab-pane label="鞋包配饰">鞋包配饰</el-tab-pane>
-      <el-tab-pane label="卫浴个护">卫浴个护</el-tab-pane>
-      <el-tab-pane label="家居生活">家居生活</el-tab-pane>
-      <el-tab-pane label="办公通勤">办公通勤</el-tab-pane>
-    </el-tabs>
+        <el-tab-pane :label="item.name" v-for="item in datalist" :key="item.name">
+          <ul class>
+            <li
+              class="list"
+              v-for="item1 in item.children"
+              :key="item1.name"
+              @click="goto('/Classifylist')"
+            >
+              <img :src="item1.image" />
+              <p>{{item1.name}}</p>
+            </li>
+          </ul>
+        </el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
@@ -27,63 +27,54 @@
 
 
 <script>
-
 export default {
-  data(){
-    return{
-      value:'',
-      
+  data() {
+    return {
+      value: "",
+      datalist: []
     };
   },
-  // methods:{
-  //   goto(path){
-  //     this.$router.push(path)
-  //   }
-  // },async created(){
-  //    let {data} = await this.$axios.get('https://8-image.xidibuy.com/shop/shop.8c0602c6ef7be9d74f806593d320d6aa0a9b93739a306e2b3500842dd515aa3d.jpeg/1500x1500/240/webp');
-  
-  //   window.console.log(data)
-  
-  // }
-
-
-
-
-  
-
+  methods: {
+    goto(path) {
+      this.$router.push(path);
+    }
+  },
+  async created() {
+    let { data } = await this.$axios.get("http://localhost:1910/fenlei");
+    this.datalist = data[0].data;
+    let datas = data[0].data.cats;
+    window.console.log(datas);
+    this.datalist = datas;
+    //  window.console.log(this.datalist)
+  }
 };
 </script>
 <style lang="scss" scoped>
-
-.el-tabs{
-    width: 100%;
-    float: left;
-    // background-color: #f3f3f3;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    top: 3.16667rem;
+.el-tabs {
+  width: 100%;
+  float: left;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  top: 3.16667rem;
 }
 
 // 列表
-.list{
-     width: 79px;
-    float: left;
-    display: block;
-    text-align: center;
-    padding-right: 12px;
-    box-sizing: border-box;
-    // padding-top: 1.66667rem;
-    padding-bottom: 0.83333rem;
-    img{
+.list {
+  width: 79px;
+  float: left;
+  display: block;
+  text-align: center;
+  padding-right: 12px;
+  box-sizing: border-box;
+  padding-bottom: 0.83333rem;
+  img {
     width: 100%;
     height: 100%;
-    }
-    p{
-    font-size:  0.83333rem;
+  }
+  p {
+    font-size: 0.83333rem;
     padding-top: 0.83333rem;
-    }
-    
+  }
 }
-
 </style>
--- src/pages/Classifylist.vue
+++ src/pages/Classifylist.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header>
-      <i class="el-icon-arrow-left"></i>
+      <i class="el-icon-arrow-left" @click="goto('/Classify')"></i>
       <p>锅具</p>
       <i class="el-icon-search"></i>
       <i class="el-icon-shopping-cart-full"></i>
@@ -13,97 +13,106 @@
             <li>综合</li>
             <li>销量</li>
             <li>新品</li>
-            <li>价格<i class="el-icon-arrow-up"></i><i class="el-icon-arrow-down"></i></li>
-         </div>
-            <div v-for="item in 6" :key="item" class="goods" > 
-              <img src="https://7-image.xidibuy.com/shop/shop.11e90879427effdb3a75267cde81258df683c9bf7cd8cc7f3500842dd515aa3d.jpeg/800x800/360/webp" alt="">
-              <a href="">日本原产sajitoki佐治陶器万古烧陶土锅砂锅炖锅汤锅煮锅9号</a>
-              <p class="text01">¥258.69</p>
-              <p class="text02">日本</p>
-            </div>
+            <li>
+              价格
+              <i class="el-icon-arrow-up"></i>
+              <i class="el-icon-arrow-down"></i>
+            </li>
+          </div>
+          <!-- 全部 -->
+          <div v-for="item in datalist.lists" :key="item.id" class="goods">
+            <img :src="item.attrImg" alt />
+            <a href>{{item.name}}</a>
+            <p class="text01">{{item.price}}</p>
+            <p class="text02">{{item.countryName}}</p>
+          </div>
         </el-tab-pane>
-
-
-
-        <el-tab-pane label="卡包">
-         <div class="show">
+        <el-tab-pane :label="item.name" v-for="item in datalist.tabs" :key="item.filterId">
+          <div class="show">
             <li>综合</li>
             <li>销量</li>
             <li>新品</li>
-            <li>价格<i class="el-icon-arrow-up"></i><i class="el-icon-arrow-down"></i></li>
-         </div>
-          消息中心</el-tab-pane>
-        <el-tab-pane label="角色管理">
-            <div class="show">
-            <li>综合</li>
-            <li>销量</li>
-            <li>新品</li>
-            <li>价格<i class="el-icon-arrow-up"></i><i class="el-icon-arrow-down"></i></li>
-         </div>
-          角色管理</el-tab-pane>
-        <el-tab-pane label="定时任务补偿">
-            <div class="show">
-            <li>综合</li>
-            <li>销量</li>
-            <li>新品</li>
-            <li>价格<i class="el-icon-arrow-up"></i><i class="el-icon-arrow-down"></i></li>
-         </div>
-          定时任务补偿</el-tab-pane>
+            <li>
+              价格
+              <i class="el-icon-arrow-up"></i>
+              <i class="el-icon-arrow-down"></i>
+            </li>
+          </div>
+          <!-- 全部 -->
+          <div v-for="item in datalist.lists" :key="item.id" class="goods">
+            <img :src="item.attrImg" alt />
+            <a href>{{item.name}}</a>
+            <p class="text01">{{item.price}}</p>
+            <p class="text02">{{item.countryName}}</p>
+          </div>
+        </el-tab-pane>
       </el-tabs>
-      <!-- <ul class="nav-list">
-        <li>全部</li>
-        <li>汤锅</li>
-        <li>煎锅/平底锅</li>
-        <li>锅具配件</li>
-        <li>炒锅</li>
-        <li>烤锅</li>
-        <li>高压锅/压力锅</li>
-        <li>奶锅</li>
-        <li>蒸锅/深烧锅</li>
-        <li>珐琅铸铁锅</li>
-        <li>塔吉锅</li>
-        <li>水壶</li>
-        <li>砂锅</li>
-        <li>微波炉配件</li>
-        <li>烘焙模具</li>
-        <li>炸锅</li>
-        <li>锅具套装</li>
-      </ul> -->
     </nav>
-    
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      value: "",
+      datalist: []
+    };
+  },
+
+  methods: {
+    goto(path) {
+      this.$router.push(path);
+    }
+  },
+  async created() {
+    let {
+      data: { data }
+    } = await this.$axios.get(
+      "https://api.m.xidibuy.com/v2/aggregation/home?cid=890&title=%E9%94%85%E5%85%B7&from=1&order=0&filterId=0&page=1&token="
+    );
+
+    this.datalist = data;
+    window.console.log(this.datalist);
+    // let top=data.tabs
+    // this.tabs=top;
+    // window.console.log(top)
+
+    // let goods=data.lists
+    // this.lists=goods;
+    // window.console.log(goods)
+
+    // let{data1}=await this.$axios.get('https://analysis.xidibuy.com/piwik.php?action_name=%E5%96%9C%E5%9C%B0%E2%80%94%E7%BA%AF%E8%BF%9B%E5%8F%A3%E5%AE%B6%E5%B1%85%E7%94%9F%E6%B4%BB%E7%94%A8%E5%93%81%E8%B4%AD%E4%B9%B0%E5%B9%B3%E5%8F%B0&idsite=3&rec=1&r=245976&h=10&m=1&s=18&url=https%3A%2F%2Fm.xidibuy.com%2F&_id=86c9d150cbbd7088&_idts=1573563250&_idvc=16&_idn=0&_refts=0&_viewts=1573778847&send_image=1&cookie=1&res=375x667&cvar=%7B%225%22%3A%5B%22_pkc%22%2C%22%E5%88%86%E7%B1%BB%E5%90%8D%E7%A7%B0%22%5D%7D&gt_ms=589&pv_id=jEaM4s');
+    // window.console.log(data1);
+  }
+};
 </script>
 <style lang="scss" scoped>
+header {
+  width: 240px;
+  height: 48px;
+  position: relative;
+  top: 0;
+  z-index: 995;
 
-header{
-    width: 240px;
-    height: 48px;
-    position: relative;
-    top: 0;
-    z-index: 995;
-    
-    i{
+  i {
     font-size: 1.6rem;
     line-height: 48px;
     color: #999;
     width: 4rem;
-    }
-  .el-icon-arrow-left{
+  }
+  .el-icon-arrow-left {
     width: 10px;
     position: absolute;
     left: 10px;
     line-height: 48px;
     color: #999;
   }
-  p{
+  p {
     font-size: 1.1rem;
     color: #333;
     text-align: center;
     display: block;
-    width:10rem;
+    width: 10rem;
     box-sizing: border-box;
     overflow: hidden;
     height: 48px;
@@ -112,76 +121,82 @@ header{
     top: 0px;
     left: 120px;
   }
-  .el-icon-search{
+  .el-icon-search {
     position: absolute;
     top: -2px;
     right: -120px;
-    
   }
-  .el-icon-shopping-cart-full{
-  position: absolute;
-  top: -2px;
-  right: -150px;
+  .el-icon-shopping-cart-full {
+    position: absolute;
+    top: -2px;
+    right: -150px;
   }
 }
 
 //导航
-.el-tab-pane label{
+.el-tab-pane label {
   width: 100%;
 }
 
-.show{
-    // color: #00bebf;
-    // font-weight: bold;
-    width: 100%;
-    height: 30px;
+.show {
+  // color: #00bebf;
+  // font-weight: bold;
+  width: 100%;
+  height: 30px;
+  float: left;
+  // background: pink;
+  li {
     float: left;
-    // background: pink;
-    li{
-      float: left;
-      padding: 0 26px 0 20px;
-      font-size: 16px
-    }
-    .el-icon-arrow-up{
-      position: relative;
-      top: -4px;
-      left: 10px;
-    }
-    .el-icon-arrow-down{
-      position: relative;
-      top: 7px;
-      left: -6px;
-    }
+    padding: 0 26px 0 20px;
+    font-size: 16px;
+  }
+  .el-icon-arrow-up {
+    position: relative;
+    top: -4px;
+    left: 10px;
+  }
+  .el-icon-arrow-down {
+    position: relative;
+    top: 7px;
+    left: -6px;
+  }
 }
 
 // 列表
-.goods{
-    width: 50%;
-    border-bottom: 1px solid #f3f3f3;
-    border-right: 1px solid #f3f3f3;
-    display: block;
-    float: left;
-    box-sizing: border-box;
-    padding: 12px;
-    padding-top: 0;
-    img{
-      width: 100%;
-    }
-    a{
-      color: #000;
-      font-size: 11.5px;
-    }
-    .text01{
-      font-size: 12px;
-      color: #00bebf;
-    }
-    .text02{
-      font-size: 12px;
-      color: #777;
-      position: relative;
-      right: -130px;
-      top: -16px;
-    }
+.goods {
+  width: 50%;
+  border-bottom: 1px solid #f3f3f3;
+  border-right: 1px solid #f3f3f3;
+  display: block;
+  float: left;
+  box-sizing: border-box;
+  padding: 12px;
+  padding-top: 0;
+  img {
+    width: 100%;
+  }
+  a {
+    color: #000;
+    font-size: 11.5px;
+    overflow: hidden;
+    padding-top: 12px;
+    width: 100%;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    text-overflow: hidden;
+  }
+  .text01 {
+    font-size: 12px;
+    color: #00bebf;
+    padding-top: 12px;
+  }
+  .text02 {
+    font-size: 12px;
+    color: #777;
+    position: relative;
+    right: -130px;
+    top: -16px;
+  }
 }
-
 </style>
