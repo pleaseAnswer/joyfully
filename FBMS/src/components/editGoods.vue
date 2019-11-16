7b43b4261d838538453577045b8ac4865c32713b
pleaseAnswer <1556998164@qq.com>
2019-11-16 17:14:21 +0800

    pleaseAnswer 后台管理系统大框架ok

---
+++ FBMS/src/components/editGoods.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        修改商品
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style lang="scss" scoped>
+ 
+</style>
---
+++ FBMS/src/components/editUser.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        修改用户
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style lang="scss" scoped>
+   
+</style>
