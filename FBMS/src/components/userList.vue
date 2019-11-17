5b2d186aca0592385a167f17211501fcc07dbbc0
pleaseAnswer <1556998164@qq.com>
2019-11-17 22:54:47 +0800

    管理员管理终极版

--- FBMS/src/components/userList.vue
+++ FBMS/src/components/userList.vue
@@ -2,22 +2,13 @@
   <div>
     <header class="userlist-head">
       <el-breadcrumb separator="/">
-<<<<<<< HEAD
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>
-          <a href="/">用户管理</a>
-        </el-breadcrumb-item>
-        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-=======
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
           <el-breadcrumb-item>用户列表</el-breadcrumb-item>
->>>>>>> a6d5d0e2d82cd7b4be043efbfb3a348ebf455400
       </el-breadcrumb>
     </header>
     <main class="userlist-main">
       <div class="userlist-button">
-<<<<<<< HEAD
         <el-button style="color:#00bebf" @click="goto">
           <i class="el-icon-circle-plus-outline" style="font-size:20px"></i>
         </el-button>
@@ -88,32 +79,7 @@
       <div class="ok" @click="ok">是</div>
       <div class="ok" @click="on">否</div>
     </el-col>
-=======
-          <el-button style="color:#00bebf" @click="goto('/addUser')"><i class="el-icon-circle-plus-outline" style="font-size:20px"></i></el-button>
-          <el-button><i class="el-icon-delete-solid" style="color:#ff5e5f; font-size:20px"></i></el-button>
-      </div>
-      <template v-if="tableData.length!=0">
-        <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
-          <el-table-column type="selection" width="55"></el-table-column>
-          <el-table-column label="用户名" prop="username" width="400"></el-table-column>
-          <el-table-column label="密码" prop="password" width="400"> </el-table-column>
-          <el-table-column label="操作" show-overflow-tooltip>
-            <el-button style="background:#00bebf; margin-right:20px"><i class="el-icon-edit" style="color:#fff;" @click="goto('/editUser')"></i></el-button>
-            <el-button style="background:#ff5e5f;"><i class="el-icon-delete-solid" style="color:#fff;"></i></el-button>
-          </el-table-column>
-        </el-table>
-      </template> 
-    </main>
-    <footer class="userlist-footer">
-      <div class="userlist-page">
-        <i class="el-icon-arrow-left" ref="refprev" @click="prev"></i>
-        <span v-for="item in pagenum" :key="item" class="pagenum" @click="changitem(item)" ref="refpage">
-          {{item}}
-        </span>
-        <i class="el-icon-arrow-right" ref="refnext" @click="next"></i>
-      </div> 
-    </footer>
->>>>>>> a6d5d0e2d82cd7b4be043efbfb3a348ebf455400
+
   </div>
 </template>
 <script>
@@ -220,7 +186,6 @@ export default {
         this.$refs.refpage[val - 1].classList.add("activepage");
       }
     },
-<<<<<<< HEAD
     async getpagenum() {
       var { data } = await this.axios.get(
         "http://localhost:1910/userlist/show"
@@ -241,52 +206,7 @@ export default {
               ele.classList.remove("activepage");
             });
             this.changitem(this.currentPage);
-=======
-    methods: {
-      toggleSelection(rows) {
-        if (rows) {
-          rows.forEach(row => {
-            this.$refs.multipleTable.toggleRowSelection(row);
-          });
-        } else {
-          this.$refs.multipleTable.clearSelection();
-        }
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },
-      goto(path){
-        this.$router.push(path);
-      },
-      async changitem(val){ 
-        this.currentPage = val;
-        var skip = (this.currentPage - 1) * this.pagesize;
-        var limit = this.pagesize;
-        var {data} = await this.axios.get(`http://localhost:1910/userlist/show?skip=${skip}&limit=${limit}`);
-        this.tableData=data;
-        if(this.$refs.refpage!=undefined){
-          this.$refs.refpage.map(ele=>{
-            ele.classList.remove("activepage");
-          })
-          this.$refs.refpage[val-1].classList.add("activepage")
-        }
-      },
-      async getpagenum(){
-        var {data} = await this.axios.get("http://localhost:1910/userlist/show");
-        this.pagenum = parseInt((data.length-1) / this.pagesize) + 1;
-      },
-      prev(){
-        if(this.$refs.refprev!=undefined && this.$refs.refnext!=undefined && this.pagenum != 1){
-          if(this.currentPage != 1){
-            this.$refs.refprev.classList.remove("disableskip");
-            this.currentPage = this.currentPage - 1;
-            if(this.$refs.refpage!=undefined){
-              this.$refs.refpage.map(ele=>{
-                ele.classList.remove("activepage");
-              })
-              this.changitem(this.currentPage);
-            }
->>>>>>> a6d5d0e2d82cd7b4be043efbfb3a348ebf455400
+
           }
         }
       }
@@ -313,7 +233,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-<<<<<<< HEAD
 .userlist-head {
   width: 100%;
   height: 40px;
@@ -347,46 +266,10 @@ export default {
       padding: 0 5px;
     }
     .pagenum {
-=======
-.userlist-head{
-  width: 100%;
-  height: 40px;
-  .el-breadcrumb{
-      padding: 5px 20px;
-      font-size: 18px;
-      line-height: 40px;
-      // background: #963;
-  }
-}
-.userlist-main{
-    width: 100%;
-    height: calc(100% - 40px);
-    .userlist-button{
-        padding: 25px 0 10px 15px;
-        text-align: left;
-    }
-    .el-table th .cell{
-        text-align: center;
-    }
-}
-.userlist-footer{
-    width: 100%;
-    height: 40px;
-    position: fixed;
-    bottom: 20px;
-    left: 0;
-  .userlist-page{
-    text-align: center;
-    i {
-      padding: 0 5px;
-    }
-    .pagenum{
->>>>>>> a6d5d0e2d82cd7b4be043efbfb3a348ebf455400
       padding: 0 5px;
       cursor: pointer;
     }
   }
-<<<<<<< HEAD
   .activepage {
     color: #00bebf;
   }
@@ -430,7 +313,6 @@ export default {
  
 }
 }
-
 .chengxu {
     width: 368px;
     height: 127px;
@@ -462,13 +344,5 @@ export default {
       float: left;
     }
   }
-=======
-  .activepage{
-    color: #00bebf;
-  }
-  .disableskip{
-    cursor: not-allowed;
-  }
-}
->>>>>>> a6d5d0e2d82cd7b4be043efbfb3a348ebf455400
+
 </style>
